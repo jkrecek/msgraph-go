eb45db19a396eaa316195f7f665fd33c9513b0d0
Honza Křeček <killerfrca@gmail.com>
2017-03-14 14:47:04 +0100

    Update event entity
    Signed-off-by: Honza Křeček <killerfrca@gmail.com>

--- entity_event.go
+++ entity_event.go
@@ -2,14 +2,15 @@ package graph
 
 type Event struct {
 	Id                   string       `json:"id,omitempty"`
-	CreatedDateTime      *flatTime     `json:"createdDateTime,omitempty"`
-	LastModifiedDateTime *flatTime     `json:"lastModifiedDateTime,omitempty"`
+	CreatedDateTime      *flatTime    `json:"createdDateTime,omitempty"`
+	LastModifiedDateTime *flatTime    `json:"lastModifiedDateTime,omitempty"`
 	ChangeKey            string       `json:"changeKey,omitempty"`
 	Subject              string       `json:"subject,omitempty"`
 	Body                 body         `json:"body,omitempty"`
 	Start                timeTimezone `json:"start,omitempty"`
 	End                  timeTimezone `json:"end,omitempty"`
-	Location             *location     `json:"location,omitempty"`
-	Recurrence           *recurrence  `json:"recurrence"`
+	Location             *location    `json:"location,omitempty"`
+	Recurrence           *recurrence  `json:"recurrence,omitempty"`
+	IsAllDay             bool         `json:"isAllDay,omitempty"`
 	// TODO more properties
 }
